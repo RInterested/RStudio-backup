1394b4cee285ee3437f4e56a335cb8201271fc8f
RInterested <antoni.parellada@gmail.com>
2023-05-20 18:37:29 -0400

    new

---
+++ inequality and violence.R
@@ -0,0 +1,27 @@
+# After this tweet https://twitter.com/blair_fix/status/1658254935769886721?s=20
+
+d = read.csv("ineq_and_violence.csv")
+head(d)
+colnames(d) = c("country", "inequality", "murders")
+plot(log(murders) ~ inequality, d, pch = 19)
+mod = lm(log(murders) ~ inequality, d)
+
+summary(mod)
+
+
+par(mfrow=c(4,2), mai = c(0.4, 0.7, 0.3, 0.11))
+
+plot(murders ~ inequality, log="y", d, pch = 19, cex=0)
+text(d$murders ~ d$inequality, labels=d$country, cex=0.8,
+     col=ifelse(d$country %in% c('QA', 'BH','MZ'),2,1))
+
+hist(residuals(mod), border=F)
+
+plot(mod, 1:3, pch=19, cex.lab=1, cex.main=0.1, 
+     cex=0.1, labels.id=d$country)
+
+plot(mod, 4:6, pch=19, cex.lab=1, cex.main=0.1, 
+     cex=ifelse(d$country %in% c('QA', 'BH','MZ'), 2, 0.1), 
+     labels.id=d$country, 
+     col=ifelse(d$country %in% c('QA', 'BH','MZ'),2,1))
+
